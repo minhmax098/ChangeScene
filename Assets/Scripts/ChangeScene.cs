272b4f414b2f1e874105138b6f28c791a5c7edbc
minhmax098 <hocminh1998@gmail.com>
2021-03-15 00:10:13 +0700

    commit first

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems; 
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    public void Changemenuscene (string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
